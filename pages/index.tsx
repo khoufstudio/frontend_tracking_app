97c957ed0c2c2e6e65d513bf35e9d01f0afe4d17
khoufstudio <rizqy.nurhaqy@gmail.com>
2022-05-20 03:51:48 +0700

    add login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <h1>test</h1>
+    <h1>Home</h1>
   )
 }
 
---
+++ pages/login.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next'
+
+const Login: NextPage = () => {
+  return (
+    <div className='bg-gray-200 h-screen'>
+
+      {/* form login */}
+      <div className='container p-8'>
+        <div className='bg-white flex flex-col w-96 mx-auto p-7 mt-10'>
+          <p className='text-center mb-4 font-bold text-2xl'>Login</p>
+          <div className="mb-3">
+            <input className='border p-3 w-full' type="text" name='username' placeholder='Username' />
+          </div>
+          <div className="mb-3">
+            <input className='border p-3 w-full' type="text" name='password' placeholder='Password' />
+          </div>
+          <button className='bg-blue-800 p-3 font-bold hover:bg-gray-500 text-white' type="submit">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
