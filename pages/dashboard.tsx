be3927eff9adf83c940173cd9be0abd20e2fe3b8
khoufstudio <rizqy.nurhaqy@gmail.com>
2022-05-25 07:57:43 +0700

    update dashboard admin

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,21 +2,14 @@ import { NextPage } from 'next'
 
 const Dashboard: NextPage = () => {
     return (
-        <div className='bg-gray-50 h-screen'>
-          <nav className='bg-white border-solid border-b-2 border-b-slate-200 p-3'>
-            <div className="flex justify-between">
-            <a className='font-bold' href="#">Logo</a>
-            <div>
-              <ul>
-                <li></li>
-              </ul>
-            </div>
-            </div>
-          </nav>
+        <div className='bg-blue-50 h-screen flex'>
           <div className='flex'>
             {/* sidebar */}
-            <div>
-              <nav className='bg-slate-700 w-56 text-white'>
+            <div className='bg-slate-800 w-56 text-white h-screen'>
+              <div className="pl-5 pr10 py-3">
+                <a className='font-bold text-lg' href="#">Logo</a>
+              </div> 
+              <nav>
                 <ul>
                   <li className='pl-5 pr-10 py-3 hover:cursor-pointer hover:bg-slate-600'>Dashboard</li>
                   <li className='pl-5 pr-10 py-3 hover:cursor-pointer hover:bg-slate-600'>Profile</li>
@@ -27,8 +20,68 @@ const Dashboard: NextPage = () => {
             </div>
             {/* end of sidebar */}
           </div>
+
           {/* main page */}
-          <div></div>
+          <div className='w-full'>
+            <nav className='bg-white border-solid border-b-2 border-b-slate-300 p-3 flex justify-end'>
+              <ul>
+                <li className='text-slate-500 font-semibold'>John Doe</li>
+              </ul>
+            </nav>
+            <main className='p-5'>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+              <p>test</p>
+            </main>
+          </div>
           {/* end of main page */}
         </div>
     )
