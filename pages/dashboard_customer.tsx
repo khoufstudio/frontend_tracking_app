d0c22db135bc4e4512788849616e70b73997e14d
khoufstudio <rizqy.nurhaqy@gmail.com>
2022-05-26 17:22:28 +0700

    add button user top right

--- pages/dashboard_customer.tsx
+++ pages/dashboard_customer.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from 'next'
 import { ChevronDownIcon, CalculatorIcon, CurrencyDollarIcon, CheckCircleIcon } from "@heroicons/react/solid";
-import { Menu } from '@headlessui/react';
+import { Menu, Transition } from '@headlessui/react';
 
 const DashboardCustomer: NextPage = () => {
   return (
@@ -26,24 +26,47 @@ const DashboardCustomer: NextPage = () => {
       {/* header */}
       <nav className='bg-white border-b-slate-300 p-3 fixed w-screen shadow-md shadow-slate-100'>
         <ul className='flex justify-end'>
-          <li className='items-center flex font-semibold justify-end text-slate-500 w-56 hover:cursor-pointer'>
+          <li>
             <Menu>
-              <Menu.Button>
-                <span>Pelanggan</span>
-                <ChevronDownIcon className='h-5 w-5 text-slate-500 pt-1'/> 
-              </Menu.Button>
-              <Menu.Items>
-                <Menu.Item>
-                  {({active}) => (
-                    <a 
-                      className={`${active && 'bg-blue-500'}`}
-                      href='/logout'
-                    >
-                      Logout
-                    </a>
-                  )}
-                </Menu.Item>
-              </Menu.Items>
+              <div className="relative">
+                <Menu.Button className='items-center flex font-semibold justify-end text-slate-500 w-56 hover:cursor-pointer'>
+                  <span>Pelanggan</span>
+                  <ChevronDownIcon className='h-5 w-5 text-slate-500 pt-1'/> 
+                </Menu.Button>
+                
+                {/* Use the Transition component. */}
+                <Transition
+                  enter="transition duration-100 ease-out"
+                  enterFrom="transform scale-95 opacity-0"
+                  enterTo="transform scale-100 opacity-100"
+                  leave="transition duration-75 ease-out"
+                  leaveFrom="transform scale-100 opacity-100"
+                  leaveTo="transform scale-95 opacity-0"
+                >
+                  <Menu.Items className='absolute left-0 w-full text-slate-500 shadow'>
+                    <Menu.Item>
+                      {({active}) => (
+                        <a 
+                          className={`${active ? 'bg-slate-100' : 'bg-white'}` + ' block p-3 border-b-2 border-b-slate-100'}
+                          href='/pengaturan'
+                        >
+                          Pengaturan
+                        </a>
+                      )}
+                    </Menu.Item>
+                    <Menu.Item>
+                      {({active}) => (
+                        <a 
+                          className={`${active ? 'bg-slate-100' : 'bg-white'}` + ' block p-3'}
+                          href='/login'
+                        >
+                          Keluar
+                        </a>
+                      )}
+                    </Menu.Item>
+                  </Menu.Items>
+                </Transition>
+              </div>
             </Menu>
           </li>
         </ul>
