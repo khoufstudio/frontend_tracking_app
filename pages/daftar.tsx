85d241d69020ba3158e76e8c43c169ea8650f319
khoufstudio <rizqy.nurhaqy@gmail.com>
2022-05-26 10:22:24 +0700

    add halaman daftar

---
+++ pages/daftar.tsx
@@ -0,0 +1,35 @@
+import type { NextPage } from 'next'
+
+const Daftar: NextPage = () => {
+  return (
+    <div className='bg-cyan-500 h-screen'>
+
+      {/* form login */}
+      <div className='p-8'>
+        <div className='bg-white flex flex-col w-96 mx-auto p-7 mt-10 drop-shadow-md rounded'>
+          <p className='text-center mb-4 font-bold text-2xl text-slate-600 tracking-wider'>Daftar</p>
+          <div className="mb-3">
+            <input className='border p-3 w-full' type="text" name='nama_perusahaan' placeholder='Nama Perusahaan' />
+          </div>
+          <div className="mb-3">
+            <input className='border p-3 w-full' type="text" name='username' placeholder='Username' />
+          </div>
+          <div className="mb-3">
+            <input className='border p-3 w-full' type="email" name='username' placeholder='Email' />
+          </div>
+          <div className="mb-3">
+            <input className='border p-3 w-full' type="password" name='password' placeholder='Password' />
+          </div>
+          <div className="mb-3">
+            <input className='border p-3 w-full' type="password_confirmation" name='password' placeholder='Ulangi Password' />
+          </div>
+          <a href='/login' className='bg-sky-600 hover:bg-sky-700 p-3 font-bold text-white text-center rounded'>
+            Daftar
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Daftar
