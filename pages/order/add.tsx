e1443a25b6fa051f47314f9dd6fcb50a7b894dd9
khoufstudio <rizqy.nurhaqy@gmail.com>
2022-06-25 12:50:59 +0700

    add pesanan page in progress

--- pages/order/add.tsx
+++ pages/order/add.tsx
@@ -1,7 +1,5 @@
-import { CalculatorIcon, CurrencyDollarIcon, CheckCircleIcon } from "@heroicons/react/solid";
 import { ReactElement } from 'react';
 import AdminLayout from "../../components/layouts/admin";
-
 
 const AddOrder = () => {
   return (
@@ -17,8 +15,59 @@ const AddOrder = () => {
             <label className="block" htmlFor="order_date">Tanggal Masuk</label>
             <input className="border-2 p-2 text-sm" id="order_date" type="date" />
           </div>
+          <div>
+            <label className="block" htmlFor="kepada">Kepada</label>
+            <input className="border-2 p-2 text-sm" id="kepada" type="text" value="" />
+          </div>
+          <div>
+            <label className="block" htmlFor="kontak_person">Kontak Person</label>
+            <input className="border-2 p-2 text-sm" id="kontak_person" type="text" value="" />
+          </div>
+          <div>
+            <label className="block" htmlFor="alamat">Alamat</label>
+            <input className="border-2 p-2 text-sm" id="alamat" type="text" value="" />
+          </div>
+          <div>
+            <label className="block" htmlFor="telepon">Telepon/Hp</label>
+            <input className="border-2 p-2 text-sm" id="telepon" type="text" value="" />
+          </div>
+          <div>
+            <label className="block" htmlFor="email">Email</label>
+            <input className="border-2 p-2 text-sm" id="email" type="text" value="" />
+          </div>
+          <div>
+            <label className="block" htmlFor="alamat_sertifkat">Alamat Sertifikat</label>
+            <input className="border-2 p-2 text-sm" id="alamat_sertifkat" type="text" value="" />
+          </div>
+          <div>
+            <label className="block" htmlFor="alamat_pengiringan_sertifkat">Alamat Pengiringan Sertifikat</label>
+            <input className="border-2 p-2 text-sm" id="alamat_pengiringan_sertifkat" type="text" value="" />
+          </div>
+          <div>
+            <label className="block" htmlFor="no_npwp">No. NPWP</label>
+            <input className="border-2 p-2 text-sm" id="no_npwp" type="text" value="" />
+          </div>
+          <div>
+            <label className="block" htmlFor="alamat_npwp">Alamat NPWP</label>
+            <input className="border-2 p-2 text-sm" id="alamat_npwp" type="text" value="" />
+          </div>
+          <div>
+            <label className="block" htmlFor="alamat_tagihan">Alamat Tagihan</label>
+            <input className="border-2 p-2 text-sm" id="alamat_tagihan" type="text" value="" />
+          </div>
+          <div>
+            <label className="block" htmlFor="kontak_person_keuangan">Kontak Person Bag. Keuangan</label>
+            <input className="border-2 p-2 text-sm" id="kontak_person_keuangan" type="text" value="" />
+          </div>
+          <div>
+            <label className="block" htmlFor="status">Status PO/Non PO</label>
+            <input className="border-2 p-2 text-sm" id="status" type="text" value="" />
+          </div>
         </div>
-        <button className="bg-blue-600 p-2 px-4 text-white rounded" type="submit">Simpan</button>
+        <div className="flex gap-2">
+          <button className="bg-gray-400 p-2 px-4 text-white rounded">Batal</button>
+          <button className="bg-blue-600 p-2 px-4 text-white rounded" type="submit">Simpan</button>
+        </div>
       </form>
     </>
   )
